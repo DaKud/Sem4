c1ea25e931bef0cfcd86a7fcdbfd8746f6fcf25d
DaKu <do.holyada@gmail.com>
2022-06-18 14:43:56 +0300

    T3

---
+++ T3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+
+int fact(int a)
+{
+    int index = 1;
+    int result = 1;
+    while (index <= a)
+    {
+        result = result * index;
+        index++;
+    }
+    return result;
+}
+
+Console.WriteLine ("insert number: ");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine (fact(num));
---
+++ T5/Program.cs
@@ -0,0 +1,15 @@
+﻿int NumberSum(int A)
+{
+    int sum = 0;
+    while ( A != 0)
+    {
+        sum += A % 10;
+        A /= 10;
+    }
+    return sum;
+}
+
+Console.WriteLine("Insert number: ");
+int A = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Sum of digits in the number "+ A + "=" + NumberSum(A));
