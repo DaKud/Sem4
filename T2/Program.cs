f455b6a13e572c517256cfd9090e91844b8db130
DaKu <do.holyada@gmail.com>
2022-06-18 12:47:49 +0300

    T2

---
+++ T2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5 
+
+// int func(int size)
+// {
+//     int sum = 0;
+//     for (int i = 0; i<=size; i++)
+//     {
+//         sum += i;
+//     }
+//     return sum;
+// }
+
+Console.WriteLine ("insert number: ");
+int A = int.Parse(Console.ReadLine());
+int digit(int A)
+{
+    int count = 0;
+
+    while (A != 0)
+    {
+    A = A / 10;
+    count++;
+    }
+
+    return count;
+}
+Console.WriteLine(digit(A));
