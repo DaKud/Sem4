33e73f3835934d9aaef58e1a3e2685e287c157a1
DaKu <do.holyada@gmail.com>
2022-06-18 20:56:54 +0300

    T4

--- T4/Program.cs
+++ T4/Program.cs
@@ -1,1 +1,22 @@
 ﻿
+// Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int[] array= new int[8];  //размер в [] 
+void print (int[] arr1)
+{
+    for (int  i=0; i< arr1.Length; i++)
+{
+    Console.WriteLine(arr1[i]+"");
+}
+Console.WriteLine();
+}
+
+for (int  i=0; i< array.Length; i++)
+{
+    array[i]=new Random().Next(0,2);
+}
+
+print (array);
